1956b2e9846ce0f7d5d758f2be2a6dc3d0a1f875
Benjamin Naidoo <naidoobenjamin1313@gmail.com>
2026-08-18 21:48:12 +0200

    Added C# Conditionals project files

---
+++ ConditionalsHomeworkApp/ConditionalsHomework/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Console.Write("Enter your first name: ");
+string? firstName = Console.ReadLine();
+
+// switch implementation
+switch (firstName.ToLower())
+{
+    case "timothy":
+    case "tim":
+        Console.WriteLine("Hello Professor");
+        break;
+    default:
+        Console.WriteLine("You are a student");
+        break;
+
+}
+
+// if implementation
+if (firstName.ToLower() == "tim" ||
+    firstName.ToLower() == "timothy")
+{
+    Console.WriteLine("Hello Professor");
+}
+else
+{
+    Console.WriteLine("You are a student");
+}
